91b11cc7371ea6276c0564fb5d8468cf847fcef9
watarunakade <wataru092783@gmail.com>
2023-02-23 21:52:04 +0900

    ビューファイル、テストコードの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Internet.password '1'"a"}
     password_confirmation {password}
-    family_name           {"hoge"}
-    first_name            {"hoge"}
+    family_name           {"ほげ"}
+    first_name            {"ほげ"}
     kana_family_name      {"ホゲ"}
     kana_first_name       {"ホゲ"}
     birthday              {'1999/01/01'}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,16 +37,57 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include ("Password can't be blank")
       end
       it 'パスワードは、6文字以上での入力が必須である' do
-        @user.password = '12345'
-        @user.password_confirmation = '12345'
+        @user.password = '123a'
+        @user.password_confirmation = '123a'
         @user.valid?
         expect(@user.errors.full_messages).to include ("Password is too short (minimum is 6 characters)")
       end
       it 'パスワードとパスワード（確認）は、値の一致が必須である' do
-        @user.password = '123456'
-        @user.password_confirmation = '1234567'
+        @user.password = 'a123456'
+        @user.password_confirmation = 'a1234567'
         @user.valid?
         expect(@user.errors.full_messages).to include ("Password confirmation doesn't match Password")
+      end
+      it 'パスワードは英数字混合でなければならない' do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Password is invalid")
+      end
+      it 'ニックネームは必須である' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it '名字は必須である' do
+        @user.family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
+      end
+      it '名字は、全角でなければならない' do
+        @user.family_name = 'Hoge'
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Family name is invalid")
+      end
+      it '名前は必須である' do 
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("First name can't be blank")
+      end
+      it '名前は、全角でなければならない' do
+        @user.first_name = 'Hoge'
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("First name is invalid")
+      end
+      it '名字(フリガナ)は、カタカナでなければならない' do
+        @user.kana_family_name = 'ほげ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Kana family name is invalid")
+      end
+      it '名前(フリガナ)は、カタカナでなければならない' do
+        @user.kana_first_name = 'ほげ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Kana first name is invalid")
       end
     end
   end

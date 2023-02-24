a6ff06fb3e29d6adbd301e77d20f1b89b664fb89
watarunakade <wataru092783@gmail.com>
2023-02-24 21:19:04 +0900

    バリデーション、Factorybotの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥]+\z/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥]+\z/}
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :kana_family_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :kana_first_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :birthday, presence: true

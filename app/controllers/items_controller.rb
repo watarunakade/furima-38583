774b75a51118b8a39b899b907df06137c69e0760
watarunakade <wataru092783@gmail.com>
2023-03-09 22:39:23 +0900

    ログアウト状態で出品しようとするとログイン画面へ遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
     if user_signed_in?
       @item = Item.new
     else
-      redirect_to root_path
+      redirect_to new_user_session_path
     end
   end
   def create

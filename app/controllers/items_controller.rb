e00927df524eb60714e5a5d1167d5c9f3cf3d501
watarunakade <wataru092783@gmail.com>
2023-03-17 23:01:51 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,11 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+  def show
+    @item = Item.find(params[:id])
+    @user = @item.user
+    @purchase = @item.purchase
+  end
 
   private
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :show]
-  resources :items, only: [:new, :create]
+  resources :items, only: [:new, :create, :show]
 end

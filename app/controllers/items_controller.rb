7aac9857fecd66e2732c55c46f6546b8196921c6
watarunakade <wataru092783@gmail.com>
2023-03-20 22:34:13 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -34,6 +34,19 @@ class ItemsController < ApplicationController
       redirect_to item_path(params[:id])
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if user_signed_in?
+      if current_user == @item.user
+        @item.destroy
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path
     end
   end
 
